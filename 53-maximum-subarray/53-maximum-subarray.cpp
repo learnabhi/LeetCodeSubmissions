90522753e9330038feda850d1104fdbd95ac02d9
learnabhi <107979033+learnabhi@users.noreply.github.com>
2022-06-22 11:34:48 +0530

    Time: 170 ms (38.66%), Space: 67.7 MB (53.59%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum = nums[0], maxSum = nums[0];
+        for(int i = 1;i<nums.size();i++){
+            currSum = max(nums[i] , currSum + nums[i]);
+            maxSum = max(currSum , maxSum);
+        }
+        return maxSum;
+    }
+};
