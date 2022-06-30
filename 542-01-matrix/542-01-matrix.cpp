ef72091d44c3e514493769e0f6ac3aa530ee9a41
learnabhi <107979033+learnabhi@users.noreply.github.com>
2022-06-30 11:30:08 +0530

    Time: 84 ms (88.38%), Space: 27.1 MB (99.82%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = size(mat) , n = size(mat[0]);
-        
-        queue<pair<int,int>> q;
+        int m = size(mat) , n = size(mat[0]) , INF = m + n;
         
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-                if(mat[i][j] == 0)
-                    q.push({i,j});
-                else 
-                    mat[i][j] = -1;
+                if(mat[i][j] == 0) continue;
+                int left = INF , top = INF;
+                if(j - 1 >= 0) left = mat[i][j-1];
+                if(i-1 >= 0) top = mat[i-1][j];
+                
+                mat[i][j] = min(left, top) + 1;
             }
         }
         
-        vector<int> dir = {1,0,-1,0,1};
-        
-        while(!q.empty()){
-            auto [x,y] = q.front();
-            q.pop();
-            
-            for(int i = 0;i<4;i++){
-                int xx = x + dir[i];
-                int yy = y + dir[i+1];
+        for(int i = m-1 ;i>=0;i--){
+            for(int j = n-1;j>=0;j--){
+                if(mat[i][j] == 0) continue;
+                int right = INF , bottom = INF;
+                if(i + 1 < m) bottom = mat[i+1][j];
+                if(j + 1 < n) right = mat[i][j+1];
                 
-                if(isSafe(xx,yy,m,n) && mat[xx][yy] == -1){
-                    q.push({xx,yy});
-                    mat[xx][yy] = 1 + mat[x][y];
-                }
+                mat[i][j] = min(mat[i][j] , min(right, bottom) + 1);
             }
         }
         
         return mat;
-    }
-    bool isSafe(int x, int y, int m, int n){
-        return (x >= 0 && x < m && y >=0 && y < n);
     }
 };
