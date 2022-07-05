3a7a2826b045c80e25cbfc46286c3eef6911d450
learnabhi <107979033+learnabhi@users.noreply.github.com>
2022-07-05 10:03:08 +0530

    Time: 4 ms (34.48%), Space: 6.2 MB (71.96%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int n = s.length() , m = t.length();
+        int s_len = s.length() , t_len = t.length();
         
-        stack<char> st1;
-        stack<char> st2;
+        int i = s_len - 1 , j = t_len - 1;
+        int count1 = 0 , count2 = 0;
         
-        for(auto& ch : s){
-            if(ch == '#'){
-                if(!st1.empty())
-                    st1.pop();
-            }else{
-                st1.push(ch);
+        while(i >= 0 || j >=0){
+            while(i >= 0 && (s[i] == '#' || count1 > 0)){
+                if(s[i] == '#') count1++;
+                else count1--;
+                i--;
             }
-        }
-        
-        for(auto& ch : t){
-            if(ch == '#'){
-                if(!st2.empty())
-                    st2.pop();
-            }else{
-                st2.push(ch);
-            }
-        }
-        
-        while(!st1.empty() && !st2.empty()){
-            char s_char = st1.top(); st1.pop();
-            char t_char = st2.top(); st2.pop();
             
-            if(s_char != t_char) return false;
+            while(j >= 0 && (t[j] == '#' || count2 > 0)){
+                if(t[j] == '#') count2++;
+                else count2--;
+                j--;
+            }
+            
+            if(i >= 0 && j >= 0 && s[i] == t[j]){
+                i--;
+                j--;
+            }else{
+                return (i == -1 && j == -1);
+            }
         }
         
-        return (st1.empty() && st2.empty());
+        return true;
     }
 };
