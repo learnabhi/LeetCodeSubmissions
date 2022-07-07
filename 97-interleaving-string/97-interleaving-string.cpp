f1089887a3f2ba0966299b26f097f1a0d7e06777
learnabhi <107979033+learnabhi@users.noreply.github.com>
2022-07-07 23:32:31 +0530

    Time: 3 ms (86.41%), Space: 6.3 MB (80.49%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
+        int dp[101][101];
+    bool isInterleavingUtil(string& s1, string& s2, string& s3, int i, int j){
+        if(i == s1.length() && j == s2.length()) return true;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(s1[i] == s3[i + j]){
+            bool fromS1 = isInterleavingUtil(s1,s2,s3,i+1,j);
+            dp[i][j] = fromS1 ? 1 : 0;
+            if(fromS1){
+                return true;
+            }
+        }
+        
+        if(s2[j] == s3[i + j]){
+            bool fromS2 = isInterleavingUtil(s1,s2,s3,i,j+1);
+            dp[i][j] = fromS2 ? 1 : 0;
+            if(fromS2){
+                return true;
+            }
+        }
+        dp[i][j] = 0;
+        return false;
+    }
     bool isInterleave(string s1, string s2, string s3) {
         int n = s1.length() , m = s2.length();
         int l = s3.length();
         
         if(l != (m + n)) return false;
         
+        memset(dp,-1,sizeof dp);
         
-        bool dp[n+1][m+1];
-        memset(dp,false, sizeof dp);
-        
-        for(int i = 0;i<=n;i++){
-            for(int j = 0;j<=m;j++){
-                if(i == 0 && j == 0)
-                    dp[i][j] = true;
-                else if(i == 0){
-                    dp[i][j] = dp[i][j-1] && (s2[j-1] == s3[i + j -1]);
-                }else if(j == 0){
-                    dp[i][j] = dp[i-1][j] && (s1[i-1] == s3[i + j -1]);
-                }else{
-                    dp[i][j] = ( dp[i-1][j] && (s1[i-1] == s3[i + j -1]) )
-                                || (dp[i][j-1] && (s2[j-1] == s3[i + j - 1]));
-                }
-            }
-        }
-        
-        return dp[n][m];
+        return isInterleavingUtil(s1,s2,s3,0,0);
     }
 };
