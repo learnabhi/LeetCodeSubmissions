0d0568119689b900191d51702759bd2d53574daf
learnabhi <107979033+learnabhi@users.noreply.github.com>
2022-07-12 12:08:55 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (68.55%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char,int> mapka;
+        vector<int> freq(26,0);
         
         int n = s.length();
         
         for(int i = 0;i<n;i++){
-            mapka[s[i]] = i;
+            freq[s[i] - 'a'] = i;
         }
         
         int last = 0 , start = 0;
         vector<int> ans;
         for(int i = 0;i<n;i++){
-            last = max(last , mapka[s[i]]);
+            last = max(last , freq[s[i] - 'a']);
             if(last == i){
                 ans.push_back(i - start + 1);
                 start = i + 1;
