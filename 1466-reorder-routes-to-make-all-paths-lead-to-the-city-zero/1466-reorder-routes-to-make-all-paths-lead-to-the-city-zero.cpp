d7ad94e789c4441ecd05c2b06641ded3977aee35
learnabhi <107979033+learnabhi@users.noreply.github.com>
2022-07-19 11:37:34 +0530

    Time: 485 ms (80.03%), Space: 107.7 MB (66.15%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
-        vector<vector<int>> adj(n);
+        vector<vector<pair<int,int>>> adj(n);
         
         for(int i = 0;i<n-1;i++){
-            adj[connections[i][0]].push_back(connections[i][1]);
-            adj[connections[i][1]].push_back(-connections[i][0]);
+            adj[connections[i][0]].push_back({connections[i][1] , 1});
+            adj[connections[i][1]].push_back({connections[i][0], -1});
         }
         int res = 0;
         dfs(adj,0,-1,res);
@@ -13,15 +13,15 @@ public:
         
         return res;
     }
-    void dfs(vector<vector<int>>& adj, int src, int par, int& res){
+    void dfs(vector<vector<pair<int,int>>>& adj, int src, int par, int& res){
         for(auto neigh : adj[src]){
-            if(par == abs(neigh))
+            if(par == neigh.first)
                 continue;
             
-            if(neigh > 0)
+            if(neigh.second > 0)
                 res++;
             
-            dfs(adj,abs(neigh),src,res);
+            dfs(adj,neigh.first,src,res);
         }
     }
 };
