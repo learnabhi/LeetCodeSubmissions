a84a9d83a8cbcdba67e6c5f282c9957bf35fac7c
learnabhi <107979033+learnabhi@users.noreply.github.com>
2022-07-19 21:04:19 +0530

    Time: 85 ms (33.17%), Space: 31.5 MB (69.52%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        queue<int> q;
+        q.push(start);
+        
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            
+            if(arr[curr] == 0) return true;
+            if(arr[curr] < 0) continue;
+            
+            if(curr + arr[curr] < arr.size())
+                q.push(curr + arr[curr]);
+            
+            if(curr - arr[curr] >= 0)
+                q.push(curr - arr[curr]);
+            
+            arr[curr] *= -1;
+        }
+        
+        return false;
+    }
+};
